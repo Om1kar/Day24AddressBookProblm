3c107bd70b78737b7e5e0fc117f1c2edaa407127
omkar <omkardudhewar121@gmail.com>
2022-09-13 20:16:27 +0530

    to search person in city,state in multiple address book

---
+++ AddressBookMain.java
@@ -0,0 +1,87 @@
+package Day24.UC12;
+
+import Day24.UC10.AddressBookSystem;
+
+import java.util.*;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//          create object for  AddressBookSystem class the object name as addressBook
+        AddressBookSystem addressBook = new AddressBookSystem();
+//          create object for map class object name is addressBookMap
+        Map<String, AddressBookSystem> addressBookMap = new HashMap<String, AddressBookSystem>();
+
+        while (true) {
+            System.out.println("\nWelcome to Address Book System");
+            System.out.println("1. New Address Book \n2. Select Address Book \n3. Delete Address Book \n4. Search Contact Data \n5. Viewcontact data \n6. Count contacts \n7. Sort contacts \n8. Exit");
+            System.out.print("Enter Your choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter Name of new Address Book: ");
+                    String bookName = sc.next();
+                    sc.nextLine();
+                    /*
+                     * adding bookname as a key and value is allocating
+                     * memory for addressbook object
+                     */
+                    addressBookMap.put(bookName, new AddressBookSystem());
+                    /*
+                     * call addressbookoption method with
+                     * passing key of hashmap
+                     */
+                    AddressBookSystem.addressBookOptions(addressBookMap.get(bookName));
+                    break;
+                case 2:
+                    System.out.println("List of available Address Book : ");
+                    /*
+                      retrieved keys from hashmap to show addressbooklist
+                     */
+                    Set<String> keys = addressBookMap.keySet();
+                    Iterator<String> i = keys.iterator();
+                    while (i.hasNext()) {
+                        System.out.println(i.next());
+                    }
+                    System.out.println("Enter Address Book name you want to Open : ");
+                    String name = sc.nextLine();
+                    System.out.println("Current Address Book is : " + name);
+                    /*
+                     * call method with passing address book name
+                     */
+                    AddressBookSystem.addressBookOptions(addressBookMap.get(name));
+                    break;
+                case 3:
+                    System.out.println("Enter Address Book name to be delete: ");
+                    name = sc.nextLine();
+                    /*
+                     * delete hashmap using remove function
+                     */
+                    addressBookMap.remove(name);
+                    break;
+                case 4:
+                    System.out.println("Welcome to the search option:");
+                    addressBook.searchByOptions();
+                    break;
+                case 5:
+                    System.out.println("Welcome to view By Option:");
+                    addressBook.viewByOption(addressBookMap);
+                    break;
+                case 6:
+                    System.out.println("Welcome to the counter");
+                    addressBook.countByOption();
+                    break;
+                case 7:
+                    System.out.println("Welcome to the sorting data alphabetically");
+                    addressBook.sortByOption();
+                case 8:
+                    sc.close();// for closing the program
+                    return;
+                default:
+                    System.out.println("You Entered Invalid Choice....!");
+                    break;
+            }
+        }
+    }
+}
